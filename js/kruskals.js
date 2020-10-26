85c9fdecc3122127ad003422faf852c2600c5b7e
LeahOlson <54635992+LeahOlson@users.noreply.github.com>
2020-10-26 11:50:13 -0500

    completed lab

--- js/kruskals.js
+++ js/kruskals.js
@@ -1,6 +1,37 @@
-
+/**
+ * Creates minimum spanning tree
+ * @param nodes		a dictionary of nodes
+ * @param edges		an array of edges
+ * @returns {*[]}
+ */
 function kruskalsMST(nodes, edges) {
-	// TODO: Implement Kruskal's Algorithm
-	
-	return []; // the edges in the MST
+	// instantiate new UnionFind data structure
+	let uf = new UnionFind(nodes.length);
+
+	// sort the edges
+	edges.sort(sortingFunction);
+
+	let t = [];
+	let k = 0;
+
+	while (t.length < nodes.length - 1) {
+		let edge = edges[k];
+		let representativeF = uf.find(edge.from)
+		let representativeT = uf.find(edge.to);
+
+		if (representativeT !== representativeF) {
+			uf.union(representativeT, representativeF);
+			t.push(edge);
+		}
+		k++;
+	}
+
+	return t; // the edges in the MST
+}
+
+/**
+ * comparator function
+ */
+function sortingFunction(edge1, edge2) {
+	return Number(edge1.label) - Number(edge2.label);
 }
